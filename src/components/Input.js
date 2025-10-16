1f931b83d61033659e99e61ef9668784fe59cdd3
John <marat.makhmutov@inbox.ru>
2025-10-16 15:59:46 +0500

    Delete src/components directory

--- src/components/Input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Input.css';
-
-// Компонент input с использованием forwardRef для передачи ref
-const Input = React.forwardRef((props, ref) => {
-    return <input ref={ref} className="custom-input" {...props} />;
-});
-
-export default Input;
